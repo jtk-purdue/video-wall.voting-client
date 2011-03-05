7bb6072986d48489c2605c4a97f9c6f0a33b69a9
jtkorb <jtkorb@localhost>
2011-03-05 09:08:29 -0500

    Reformatted using Eclipse -> Source -> Format command

--- src/android/button/Welcome.java
+++ src/android/button/Welcome.java
@@ -18,46 +18,49 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class Welcome extends Activity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        setContentView(R.layout.welcome);
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		setContentView(R.layout.welcome);
+	}
 
-    }
-    
-    //@Override 
-	 public boolean onPrepareOptionsMenu(Menu menu) { 
-	  return super.onPrepareOptionsMenu((android.view.Menu) menu); 
-	 } 
-	
-	  //@Override
-	    public boolean onCreateOptionsMenu(android.view.Menu menu) {
-	        MenuInflater inflater = getMenuInflater();
-	        inflater.inflate(R.menu.menu, menu);
-	        return true;
-	    }
-	    
-	    @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	        switch (item.getItemId()) {
-	            case R.id.help:     Intent i = new Intent();
-	            					i.setClass(Welcome.this, Help.class);
-	            					startActivity(i);
-									break;
-	            
-	            case R.id.info:     Intent i2 = new Intent();
-	            					i2.setClass(Welcome.this, Information.class);
-	            					startActivity(i2);
-	            					break;
-	            					
-	            case R.id.settings:     Intent i3 = new Intent();
-										i3.setClass(Welcome.this, Settings.class);
-										startActivity(i3);
-										break;
-										
-	            case R.id.quit:     finish();
-									break;
-	        }
-	        return true;
-	    }	
+	// @Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		return super.onPrepareOptionsMenu((android.view.Menu) menu);
+	}
+
+	// @Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.help:
+			Intent i = new Intent();
+			i.setClass(Welcome.this, Help.class);
+			startActivity(i);
+			break;
+
+		case R.id.info:
+			Intent i2 = new Intent();
+			i2.setClass(Welcome.this, Information.class);
+			startActivity(i2);
+			break;
+
+		case R.id.settings:
+			Intent i3 = new Intent();
+			i3.setClass(Welcome.this, Settings.class);
+			startActivity(i3);
+			break;
+
+		case R.id.quit:
+			finish();
+			break;
+		}
+		return true;
+	}
 }
