622ecd19640586d39890c4dd1e3d0aa033f8a38f
mhumayun <mhumayun@localhost>
2011-03-19 20:03:10 -0400

    Changed Vote Tab Added icons to voteItems Cleaned up code

--- src/android/button/Acknowledgements.java
+++ src/android/button/Acknowledgements.java
@@ -11,10 +11,10 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Acknowledgements extends ListActivity{
-	
+public class Acknowledgements extends ListActivity {
+
 	ArrayList<String> nameList;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -37,51 +37,51 @@ public class Acknowledgements extends ListActivity{
 		nameList.add("I");
 		nameList.add("J");
 		nameList.add("K");
-		setListAdapter(new ArrayAdapter<String>(this, R.layout.vote,
-				nameList));
-		
+		setListAdapter(new ArrayAdapter<String>(this,
+				R.layout.acknowledgements, nameList));
+
 		ListView lv = getListView();
 		lv.setTextFilterEnabled(true);
 	}
 
-//@Override
-public boolean onPrepareOptionsMenu(Menu menu) {
-	return super.onPrepareOptionsMenu((android.view.Menu) menu);
-}
-
-// @Override
-@Override
-public boolean onCreateOptionsMenu(android.view.Menu menu) {
-	MenuInflater inflater = getMenuInflater();
-	inflater.inflate(R.menu.menu, menu);
-	return true;
-}
-
-@Override
-public boolean onOptionsItemSelected(MenuItem item) {
-	switch (item.getItemId()) {
-	case R.id.help:
-		Intent i = new Intent();
-		i.setClass(Acknowledgements.this, Help.class);
-		startActivity(i);
-		break;
-
-	case R.id.stats:
-		Intent i2 = new Intent();
-		i2.setClass(Acknowledgements.this, Information.class);
-		startActivity(i2);
-		break;
-
-	case R.id.settings:
-		Intent i3 = new Intent();
-		i3.setClass(Acknowledgements.this, Settings.class);
-		startActivity(i3);
-		break;
-
-	case R.id.quit:
-		finish();
-		break;
+	// @Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		return super.onPrepareOptionsMenu((android.view.Menu) menu);
 	}
-	return true;
-}
+
+	// @Override
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.help:
+			Intent i = new Intent();
+			i.setClass(Acknowledgements.this, Help.class);
+			startActivity(i);
+			break;
+
+		case R.id.info:
+			Intent i2 = new Intent();
+			i2.setClass(Acknowledgements.this, Information.class);
+			startActivity(i2);
+			break;
+
+		case R.id.settings:
+			Intent i3 = new Intent();
+			i3.setClass(Acknowledgements.this, Settings.class);
+			startActivity(i3);
+			break;
+
+		case R.id.quit:
+			finish();
+			break;
+		}
+		return true;
+	}
 }
