34a3f400d653291d64651cff43c58dcacb261329
sohailmehra <sohailmehra@localhost>
2011-04-03 15:22:52 -0400

    Audio tab updated;

--- src/edu/purdue/cs/lawson/vw/Audio.java
+++ src/edu/purdue/cs/lawson/vw/Audio.java
@@ -2,7 +2,10 @@ package edu.purdue.cs.lawson.vw;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
@@ -13,6 +16,7 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.Gallery;
 import android.widget.Gallery.LayoutParams;
 import android.widget.TextView;
@@ -21,7 +25,7 @@ import android.widget.ImageSwitcher;
 import android.widget.ImageView;
 import android.widget.AdapterView.OnItemClickListener;
 
-public class Audio extends Activity {
+public class Audio extends Activity  {
 	Animation anim = null;
 	MediaPlayer mp;
 
@@ -65,6 +69,147 @@ public class Audio extends Activity {
 		ins = (TextView) findViewById(R.id.audio_instructions);
 		internetcheck = checkInternetConnection();
 		mp = new MediaPlayer();
+		
+		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+		Button button = (Button) findViewById(R.id.screen1);// Creating a button
+		button.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(0);
+			}
+		});
+
+		// Above process is repeated for each audio track.
+		Button button2 = (Button) findViewById(R.id.screen2);
+		button2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(1);
+			}
+		});
+
+		Button button3 = (Button) findViewById(R.id.screen3);
+		button3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(2);
+			}
+		});
+		
+		Button button4 = (Button) findViewById(R.id.screen4);// Creating a button
+		button4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(3);
+			}
+		});
+
+		// Above process is repeated for each audio track.
+		Button button5 = (Button) findViewById(R.id.screen5);
+		button5.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(4);
+			}
+		});
+
+		Button button6 = (Button) findViewById(R.id.screen6);
+		button6.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(5);
+			}
+		});
+
+		Button button7 = (Button) findViewById(R.id.screen7);// Creating a button
+		button7.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(6);
+			}
+		});
+
+		// Above process is repeated for each audio track.
+		Button button8 = (Button) findViewById(R.id.screen8);
+		button8.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(7);
+			}
+		});
+
+		Button button9 = (Button) findViewById(R.id.screen9);
+		button9.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(8);
+			}
+		});
+
+		Button button10 = (Button) findViewById(R.id.screen10);// Creating a button
+		button10.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(9);
+			}
+		});
+
+		// Above process is repeated for each audio track.
+		Button button11 = (Button) findViewById(R.id.screen11);
+		button11.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(10);
+			}
+		});
+
+		Button button12 = (Button) findViewById(R.id.screen12);
+		button12.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(11);
+			}
+		});
+
+		Button button13 = (Button) findViewById(R.id.screen13);// Creating a button
+		button13.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(12);
+			}
+		});
+
+		// Above process is repeated for each audio track.
+		Button button14 = (Button) findViewById(R.id.screen14);
+		button14.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(13);
+			}
+		});
+
+		Button button15 = (Button) findViewById(R.id.screen15);
+		button15.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(14);
+			}
+		});
+		
+		Button button16 = (Button) findViewById(R.id.screen16);
+		button16.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				playSong(15);
+			}
+		});
+
+
+		
+		
+		
+		
 		/*
 		//Gallery gallery = (Gallery) findViewById(R.id.gallery1);
 		//gallery.setAdapter(new ImageAdapter(this));
@@ -105,6 +250,23 @@ public class Audio extends Activity {
 			Log.d("Connection State", "Internet Connection Not Present");
 			return false;
 		}
+	}
+	
+	public void playSong(int position)
+	{
+		String url = songUrls[position];
+		   mp.stop();
+		   mp.reset();
+	       if (internetcheck) {
+	       try {
+	            mp.setDataSource(url);
+	            mp.prepare();
+	            mp.start();
+	       } catch (Exception e) {
+	            Log.i("Exception", "Exception in streaming mediaplayer e = " +
+	            e);
+	       }
+	      }
 	}
 	
 	public void onPause()
