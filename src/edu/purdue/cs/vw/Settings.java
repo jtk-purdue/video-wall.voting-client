ebe8217240fd6de0143c033c988d0657bba506e5
jtkorb <jtkorb@localhost>
2011-05-04 21:02:21 -0400

    Reformatted and removed unnecessary import.

--- src/edu/purdue/cs/vw/Settings.java
+++ src/edu/purdue/cs/vw/Settings.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.widget.Toast;
 
 /*This file sets up the layout for when the settings button is pressed.
@@ -20,43 +19,33 @@ public class Settings extends PreferenceActivity {
 		addPreferencesFromResource(R.layout.settings);
 		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
 
-		OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() 
-		{
-			  public void onSharedPreferenceChanged(SharedPreferences prefs, String key)
-			  {
-				  if ("editTextPref".equals(key)) {
-					    String valueString = prefs.getString(key, "");
-					    int value = Integer.parseInt(valueString);
-					    if(value > 65536)
-					    {
-					    EditTextPreference p = (EditTextPreference) findPreference(key);
-					    p.setText(""+4242);
-					    display();
-					    }
-					    
-					    else if(value < 0)
-					    {
-					    EditTextPreference p = (EditTextPreference) findPreference(key);
-					    p.setText(""+4242);
-					    display();
-					    }
+		OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+			public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+				if ("editTextPref".equals(key)) {
+					String valueString = prefs.getString(key, "");
+					int value = Integer.parseInt(valueString);
+					if (value > 65536) {
+						EditTextPreference p = (EditTextPreference) findPreference(key);
+						p.setText("" + 4242);
+						display();
 					}
-			  }
+
+					else if (value < 0) {
+						EditTextPreference p = (EditTextPreference) findPreference(key);
+						p.setText("" + 4242);
+						display();
+					}
+				}
+			}
 		};
 
-			prefs.registerOnSharedPreferenceChangeListener(listener);
+		prefs.registerOnSharedPreferenceChangeListener(listener);
 
-				
 	}
-	
-	public void display()
-	{
-		Toast t = Toast.makeText(this,"Invalid Port. Reverting to 4242. ", Toast.LENGTH_SHORT);
+
+	public void display() {
+		Toast t = Toast.makeText(this, "Invalid Port. Reverting to 4242. ", Toast.LENGTH_SHORT);
 		t.show();
 	}
-	
 
 }
-
-
-
