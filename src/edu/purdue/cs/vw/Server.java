a59c7471fa73cb7614b988002b1d48c95aa91d92
jtkorb <jtkorb@localhost>
2011-05-25 07:12:24 -0400

    Added Server interface back again.

---
+++ src/edu/purdue/cs/vw/Server.java
@@ -0,0 +1,12 @@
+package edu.purdue.cs.vw;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public interface Server {
+    public void vote(String name) throws IOException;
+
+    public ArrayList<String> getList() throws IOException;
+
+    public ArrayList<String> getCount() throws IOException;
+}
